eee552d56a8f3ca5c24a8cd1ba7c8f917b01bc79
Bulavintsev <v.bulavintsev@kt-team.de>
2022-02-05 13:04:30 +0400

    поправил контроллер юзера

--- src/main/java/ru/kata/spring/boot_security/demo/contollers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/contollers/UserController.java
@@ -1,24 +1,23 @@
 package ru.kata.spring.boot_security.demo.contollers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.UserService;
 
-import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    @Autowired
-    UserService userService;
 
-    @GetMapping("edit/{id}")
-    public String editPage(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.getUser(id));
+    @GetMapping()
+    public String showUserInfo(Model model) {
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         return "user";
     }
 }

06c16dd9e9248f9aea81b2c652349fd2372fd33f
Bulavintsev <v.bulavintsev@kt-team.de>
2022-02-05 13:01:33 +0400

    поправил репозиторий роли и сервис

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImp.java
@@ -1,25 +1,23 @@
 package ru.kata.spring.boot_security.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
-import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
-import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class RoleServiceImp implements RoleService{
     @PersistenceContext
-    private EntityManager em;
     private RoleRepository roleRepository;
-
 
     public RoleServiceImp() {
     }
 
+    @Autowired
     public RoleServiceImp(@Qualifier("RoleRepository") RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
